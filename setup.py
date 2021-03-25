aa4ecc7a1a5c7ab5ab27b5d7d45d06ac26598eaf
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2021-03-25 16:07:41 +0100

    DP-1968: Add initial project files

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="okdata-resource-auth",
+    version="0.1.0",
+    author="Oslo Origo",
+    author_email="dataplattform@oslo.kommune.no",
+    description="Dataplatform resource authorizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oslokommune/okdata-resource-auth",
+    packages=setuptools.find_namespace_packages(
+        include="origo.resource_auth.*", exclude=["tests*"]
+    ),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[],
+)
